2a93b8ee9b971b3273d20df42170cfd750a47376
bmwasonga <bmwasonga@gmail.com>
2022-03-02 01:49:05 +0300

    added scripts

--- server/server.js
+++ server/server.js
@@ -1,8 +1,23 @@
-const { sequelize } = require('../models');
+const express = require('express');
+const { sequelize, user } = require('../models');
+
+const app = express();
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+  const { phone, password, role } = req.body;
+
+  try {
+    const unser = await user.create({ phone, password, role });
+    return res.json(user);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: { error } });
+  }
+});
 
 const main = async () => {
-  // await sequelize.sync({ force: true });
-  await sequelize.sync();
+  await sequelize.sync({ force: true });
   console.log('Database synced');
   process.exit();
 };
